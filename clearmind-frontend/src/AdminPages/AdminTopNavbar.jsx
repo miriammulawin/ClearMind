b99166f874430e7fb8848a1398fda0ce97f731f0
Miriam Mulawin <mulawinmiriam75@gmail.com>
2026-01-27 16:45:13 +0800

    admin layout

---
+++ clearmind-frontend/src/AdminPages/AdminTopNavbar.jsx
@@ -0,0 +1,25 @@
+import { FiLogOut, FiSearch } from "react-icons/fi";
+import { AiFillMessage } from "react-icons/ai";
+import { IoNotifications } from "react-icons/io5";
+
+function AdminTopNavbar({ activeMenu }) {
+  return (
+    <div className="top-navbar">
+      <div className="top-navbar-left">
+        <h3>{activeMenu}</h3> 
+      </div>
+
+      <div className="top-navbar-right">
+        <AiFillMessage className="top-icon" />
+        <IoNotifications className="top-icon" />
+        <div className="search-box">
+          <input type="text" placeholder="Search" />
+          <FiSearch className="search-icon" />
+        </div>
+        <FiLogOut className="top-icon" />
+      </div>
+    </div>
+  );
+}
+
+export default AdminTopNavbar;
