d0793aeaeb16560c7a189ffaa9376d3581b783df
arac880 <ceresarachristina74@gmail.com>
2026-01-28 23:01:53 +0800

    Re-building Log In Page

--- clearmind-frontend/src/App.jsx
+++ clearmind-frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registration />} />
         <Route path="/create-accounts"element={<CreateAccounts/>} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
