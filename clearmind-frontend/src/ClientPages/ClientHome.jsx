b4623fc96a4fa42f3ce4ac33db26223832049dd1
arac880 <ceresarachristina74@gmail.com>
2026-01-29 21:01:28 +0800

    add client apppointment.jsx

--- clearmind-frontend/src/ClientPages/ClientHome.jsx
+++ clearmind-frontend/src/ClientPages/ClientHome.jsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
 import { Container, Card, Row, Col, Form, Button, Image } from "react-bootstrap";
-// import logo_login from "../assets/CMPS_Logo.png";
-import { FaEye, FaEyeSlash, FaLock} from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
 import "./ClientStyle/ClientHome.css"
 import ClientHeader from "./ClientComponents/ClientHeader";
 import ClientFooter from "./ClientComponents/ClientFooter";
