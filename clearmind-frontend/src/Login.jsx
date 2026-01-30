f94fb192b370fc119823855f6eddb1f3e8fdbdc3
arac880 <ceresarachristina74@gmail.com>
2026-01-30 11:04:03 +0800

    Update - Log In much responsive

--- clearmind-frontend/src/Login.jsx
+++ clearmind-frontend/src/Login.jsx
@@ -18,82 +18,111 @@ function Login() {
   };
 
   return (
-    <Container fluid className="vh-100 d-flex justify-content-center align-items-center bg-transparent">
-      <Card className="vw-100% card-custom d-flex justify-content-center align-items-center px-4 ">
-        <Card.Body >
-           <Container>
-            <Row>
-              <Col >
-                <Image src={logo_login} fluid className="logo d-block mx-auto" />
-              </Col>
-              <span className="span-tagline text-center">Log in to continue your journey toward a clearer, healthier mind.</span>
-            </Row>
-          </Container>
-          <Form>
-            <Form.Group className="mb-2" controlId="email">
-              <Form.Label className="login-text" >Email <span className="text-danger">*</span></Form.Label>
-                <div className="input-icon-wrapper">
-                  <MdEmail className="input-icon-left"/>
-                  <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    autoComplete="email"
-                    required
-                    size="sm"
-                    className="custom-field input-with-icon"
+    <Container fluid className="login-container bg-transparent">
+      <Row className="justify-content-center align-items-center min-vh-100 g-0 mx-0">
+        <Col xs={11} sm={10} md={7} lg={5} xl={4} xxl={3} className="login-col">
+          <Card className="card-custom">
+            <Card.Body className="card-body-responsive">
+              <Row className="g-0">
+                <Col xs={12} className="text-center logo-section">
+                  <Image 
+                    src={logo_login} 
+                    fluid 
+                    className="logo-img d-block mx-auto" 
                   />
-                </div>
-            </Form.Group>
-            
-            {/* passoword */}
-            <Form.Group className="mb-2" controlId="password" style={{ position: "relative" }}>
-              <Form.Label className="login-text">Password <span className="text-danger">*</span></Form.Label>
-                <div className="input-icon-wrapper">
-                  <FaLock className="input-icon-left"/>
-                  <Form.Control
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Enter password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    autoComplete="current-password"
+                </Col>
+                <Col xs={12} className="text-center">
+                  <p className="tagline-text">
+                    Log in to continue your journey toward a clearer, healthier mind.
+                  </p>
+                </Col>
+              </Row>
+              <Form onSubmit={handleLogin} className="login-form">
+                <Form.Group className="mb-3" controlId="email">
+                  <Form.Label className="form-label-custom">
+                    Email <span className="text-danger">*</span>
+                  </Form.Label>
+                  <div className="input-icon-wrapper">
+                    <MdEmail className="input-icon-left"/>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      autoComplete="email"
+                      required
+                      className="input-field"
+                    />
+                  </div>
+                </Form.Group>
+                
+                <Form.Group className="mb-3" controlId="password">
+                  <Form.Label className="form-label-custom">
+                    Password <span className="text-danger">*</span>
+                  </Form.Label>
+                  <div className="input-icon-wrapper">
+                    <FaLock className="input-icon-left"/>
+                    <Form.Control
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Enter password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      autoComplete="current-password"
+                      required
+                      className="input-field"
+                    />
+                    <span
+                      onClick={() => setShowPassword(!showPassword)}
+                      className="input-icon-right"
+                      role="button"
+                      tabIndex={0}
+                      onKeyPress={(e) => e.key === 'Enter' && setShowPassword(!showPassword)}
+                      aria-label={showPassword ? "Hide password" : "Show password"}
+                    >
+                      {showPassword ? <FaEye /> : <FaEyeSlash />}
+                    </span>
+                  </div>
+                </Form.Group>
+                
+                <Form.Group className="mb-3" controlId="termsCheck">
+                  <Form.Check 
+                    type="checkbox"
+                    label={
+                      <>
+                        I agree to the{' '}
+                        <a className="terms-link" href="/terms">
+                          Terms & Conditions
+                        </a>{' '}
+                        <span className="text-danger">*</span>
+                      </>
+                    }
+                    checked={agreed}
+                    onChange={(e) => setAgreed(e.target.checked)}
                     required
-                    size="sm"
-                    className="custom-field input-with-icon"
+                    className="terms-checkbox"
                   />
-                  <span
-                    onClick={() => setShowPassword(!showPassword)}
-                    className="input-icon-right"
-                  >
-                    {showPassword ? <FaEye /> : <FaEyeSlash />}
-                  </span>
-                </div>
-            </Form.Group>
-            
-            {/* terms check */}
-            <Form.Group className="mb-3 custom-checkbox" controlId="termsCheck">
-              <Form.Check 
-                type="checkbox"
-                label={<>I agree to the <a className="terms-condi" href="/terms">Terms & Conditions</a> <span className="text-danger">*</span></>}
-                checked={agreed}
-                onChange={(e) => setAgreed(e.target.checked)}
-                required
-                className="terms-check"
-              />
-            </Form.Group>
+                </Form.Group>
 
-            <Button variant="none" className="btn-login w-100">
-              <b>
-                LOG IN
-              </b>
-            </Button>
-            <p className="register-text text-center m-2" > Don’t have an account? <a className="register-link" href="/register">Register.</a>
-            </p>
-          </Form>
-        </Card.Body>
-      </Card>
-   </Container>
+                <Button 
+                  variant="primary" 
+                  type="submit"
+                  className="login-button w-100"
+                >
+                  LOG IN
+                </Button>
+                
+                <p className="register-text text-center mb-0">
+                  Don't have an account?{' '}
+                  <a className="register-link" href="/register">
+                    Register.
+                  </a>
+                </p>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
