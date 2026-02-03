78f7c5b547aba4e74f28adff062e0197f701bf18
brdlc-dev <delacruzbrian315@gmail.com>
2026-02-03 11:12:44 +0800

    pushing axios client

--- clearmind-frontend/src/api/axiosClient.js
+++ clearmind-frontend/src/api/axiosClient.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
+  // base sa url mo ung aken eto ung localhost ko nakafolder pa ng isa.
   baseURL: "http://localhost/ClearMind/clearmind-backend",
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true, // allow cookies if needed for CORS
+  withCredentials: true, 
 });
 
 export default axiosClient;
