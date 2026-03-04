2f35260f76bd542c437cd99edc815f9842a127d7
brdlc-dev <delacruzbrian315@gmail.com>
2026-03-04 09:47:24 +0800

    added in backend

--- clearmind-backend/app/Http/Controllers/AdminDashboardController.php
+++ clearmind-backend/app/Http/Controllers/AdminDashboardController.php
@@ -35,8 +35,6 @@ class AdminDashboardController extends Controller
     return response()->json($appointments);
 }
 
-public 
-
 public function statusCounts()
 {
     $counts = Appointment::selectRaw("
